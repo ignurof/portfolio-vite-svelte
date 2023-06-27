319f92cd1d425812708036842fa79864a26e4994
ignurof <ignurof@ignurof.com>
2023-06-28 01:28:28 +0200

    learning what changed with svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+	//import svelteLogo from './assets/svelte.svg'
+	//import viteLogo from '/vite.svg'
+	//import Counter from './lib/Counter.svelte'
+
+	let activePage = -1;
+	let pages = { 
+		"index": 0,
+		"about": 1
+	};
+
+	const testCall = () => {
+		console.log("Test");
+	};
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+	<nav>
+		<button>Index</button>
+		<button on:click={() => testCall()}>About</button>
+	</nav>
 
-  <div class="card">
-    <Counter />
-  </div>
+	<!-- Site Content -->
+	<!-- <Index /> -->
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<footer>
+		- ignurof
+	</footer>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
